17c71a1a98f645ec6d4c9888f20c5c2502a05cb1
shashantsah <shashant.sah@gmail.com>
2024-06-05 16:06:28 +0530

    ejs file rendering learnt

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const path = require("path");
 
 const app=express();
 const port=3000;
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'pages'));
+
 app.get('/',(req,res)=>{
-    console.log("hit one time");
+    console.log("hit from home");
     // res.send("hello");
     // res.send('<div><h2>Hello html</h2><p>hi</p></div>');
     // res.json({name:"rahul",age:24,phone:[123,234,456]});
-    res.sendFile(path.join(__dirname,'../1.jpg'));
+    // res.sendFile(path.join(__dirname,'../1.jpg'));
+    // res.redirect("https://www.google.com");
+    // res.redirect("user");
+    // res.render('/Users/shashantkumar/Desktop/my_express/pages/index.ejs', { Title: 'Homepage', message: 'Welcome!' });
+    res.render('index',{Title: 'Homepage', message: 'Welcome!'});
+
+})
+app.get('/user',(req,res)=>{
+    console.log("hit from user");
+    // res.redirect("/");
 })
 app.listen(port,()=>{
     console.log("listening on port ${port}")
